8e9204904fe00be230917219132422d1ca9c18ff
kuakling <kuakling@gmail.com>
2015-09-29 10:25:38 +0700

    First

---
+++ Gen.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace kuakling\smartmenus;
+
+/**
+ * This is just an example.
+ */
+class Gen extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
